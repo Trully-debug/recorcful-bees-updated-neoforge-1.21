e5a3652ed52de4d33e568faeb697bac5f3e7022f
Trully-debug <aeiden.brown@outlook.com>
2026-07-13 15:47:34 -0400

    Made the mod show up in NeoForge as Trully Recorcful Bees

--- src/main/java/net/trullyabunny/recorcfulbees/RecorcfulBees.java
+++ src/main/java/net/trullyabunny/recorcfulbees/RecorcfulBees.java
@@ -1,6 +1,7 @@
 package net.trullyabunny.recorcfulbees;
 
 import net.trullyabunny.recorcfulbees.block.ModBlocks;
+import net.trullyabunny.recorcfulbees.item.ModCreativeModeTab;
 import net.trullyabunny.recorcfulbees.item.ModItems;
 import org.slf4j.Logger;
 
@@ -76,23 +77,18 @@ public class RecorcfulBees {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModCreativeModeTab.register(modEventBus);
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
+
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -111,15 +107,10 @@ public class RecorcfulBees {
         Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
     }
 
+
+
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(ModItems.ironhoneycomb);
-            event.accept(ModItems.goldhoneycomb);
-            event.accept(ModBlocks.ironhoneycomb_block);
-            event.accept(ModBlocks.goldhoneycomb_block);
-        }
-    }
+
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
